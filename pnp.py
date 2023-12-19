ba56917df67a55c1e7c34ce75128e5c3c66360dd
SLG <mineslavan@gmail.com>
2023-12-19 10:50:10 +0300

    remove not needed for prj

--- pnp.py
+++ pnp.py
@@ -1,10 +1,3 @@
-##################################################################
-#################### НАСТРОЙКИ ###################################
-##################################################################
-
-MDB_PATH = r"\\smtdev\STC_DB_SMT\CarrierDB.mdb" # путь к MDB
-SMB_USERNAME = "Администратор" # логин SMB
-SMB_PASSWORD = "R&D_srv" # пароль SMB
 
 #ENCODING = 'cp1251' # Кодировка импорта и экспорта
 ENCODING = 'utf8' # Кодировка импорта и экспорта
@@ -12,11 +5,6 @@ ENCODING = 'utf8' # Кодировка импорта и экспорта
 
 #OPEN_FILES_WHEN_DONE = False # Открыть папку в конце
 OPEN_FILES_WHEN_DONE = True
-
-##################################################################
-##################################################################
-##################################################################
-
 
 
 from sys import argv
@@ -31,9 +19,6 @@ from transliterate import translit, get_available_language_codes
 import readchar
 import shutil
 
-#######LOAD MDB###########
-import pyodbc
-from smbclient import register_session
 import warnings
 warnings.filterwarnings('ignore')
 ##########################
@@ -68,26 +53,6 @@ if len(argv) <=1:
 
 os.system('cls')
 
-
-
-
-def ask_mode(modes, question):
-        txt = ''
-        for key_idx in range(len(modes)):
-            txt += f'{key_idx+1} : {modes[key_idx]}\n'
-        msg(txt, title=question, color=WHITE, cls=True)
-        key = 'abc'
-        while not key.isdigit():
-            key = readchar.readkey()
-            try:
-                key = int(key)
-                mode = modes[key - 1]
-                return key - 1
-            except:
-                key = 'abc'
-                pass
-#delete_folder = ask_mode(YESNO, 'Удалить старые файлы?')
-include_D = ask_mode(YESNO, 'Включить актив?')
 delete_folder = True
 if exists('READY/'):
     if delete_folder:
@@ -97,36 +62,6 @@ if exists('READY/'):
             error('Ошибка удаления папки', e)
 
 msg('Обработка ...', title='Обработка', color=YELLOW, cls=True)
-df_join_good = pd.DataFrame()
-df_join_bad = pd.DataFrame()
-df_join = pd.DataFrame()
-
-
-
-
-
-try:
-    register_session("smtdev", username=SMB_USERNAME, password=SMB_PASSWORD)
-    #shutil.copyfile(MDB_PATH, "CarrierDB.mdb")
-    #MDB_FILE = realpath(r'CarrierDB.mdb')
-    MDB_FILE = MDB_PATH
-    driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
-    cnxn = pyodbc.connect(f'Driver={driver};DBQ={MDB_FILE}')
-    crsr = cnxn.cursor()
-    library = pd.read_sql("SELECT * FROM CarrierTable", cnxn)
-    library = library[['CarrierId', 'ComponentName', 'Location']]
-
-except Exception as e:
-    error('Не удалось подключиться к БД SMT', e)
-
-
-try:
-    library = library[['CarrierId', 'ComponentName', 'Location']]
-    library_filtered = library[library['Location']=='STANOK'].reset_index(drop=True)
-    library_filtered['CarrierId'] = library_filtered['CarrierId'].astype('str').str.zfill(6)
-    library_filtered.rename(columns={"ComponentName": "Comment"}, inplace=True)
-except Exception as e:
-    error('файл библиотеки не читается')
 
 for i in range(len(argv)):
     if i > 0:
@@ -140,7 +75,7 @@ for i in range(len(argv)):
 
         msg(f'Проект: {Path(INPUT_FILE).stem}', cls=False)
         OUT_FILE = Path(INPUT_FILE).stem
-        OUT_FOLDER = f'READY/{OUT_FILE}/'
+        OUT_FOLDER = f'READY/'
         OUT_FILE += '.csv'
         if not exists(OUT_FOLDER):
             os.mkdir(OUT_FOLDER)
@@ -152,9 +87,6 @@ for i in range(len(argv)):
         REGEXP_VALUE_F_LFCN = re.compile(r'\d{2,4}[+]$|\d{2,4}[+]$')
         #REGEXP_IS_ACTIVE = re.compile(r'^[RCLF]-.{1,}')
         REGEXP_IS_ACTIVE = re.compile(r'\A(?![DXTQ])')
-
-        
-
 
         if exists(INPUT_FILE):
             #print(INPUT_FILE)
@@ -190,16 +122,8 @@ for i in range(len(argv)):
         csv['Translit'] = csv['Comment'].apply(check_trans)
         csv['Comment'] = csv['Comment'].apply(trans)
         print("\n".join(list(set(store.translit))))
-        
-        try:
-            csv = pd.merge(csv, library_filtered, on='Comment', how='left', sort=False)
-        except KeyError as e:
-            error('Ошибка чтения PNP', e, INPUT_FILE)
         csv.drop_duplicates(subset=['Designator'], keep='first', inplace=True)
-
-
         csv['Comment'] = csv['Comment'].str.replace(' ', '-')
-
 
         packs_l = {'L-0402': '0402_C',
                    'L-LQW15': '0402_C',
@@ -312,53 +236,11 @@ for i in range(len(argv)):
                 #print(e)
                 return
 
-
-
         csv['package'] = csv.apply(extract_package, axis=1)
         csv['value'] = csv.apply(extract_value, axis=1)
-        csv['CarrierId'] = 'R' + csv['CarrierId']
-        csv.drop(columns=['Location'], inplace=True)
 
-
-        ###############################################
-        ###############################################
-        ###############################################
         csv_pnp = csv.copy()
-        csv_comp = csv.copy()
-
-        if not include_D:
-            csv_comp = csv_comp[csv_comp.Comment.str.match(REGEXP_IS_ACTIVE)]
-        csv_comp.drop_duplicates(subset=['Comment'], keep='first', inplace=True)
-
-
-        csv_comp = csv_comp[['Comment', 'CarrierId', 'package', 'value', 'Translit']]
-        csv_comp['tip_lenty'] = ''
-        csv_comp.loc[csv_comp.package.isin(['0603_C', '0603']), 'tip_lenty'] = "Tape08-4 white"
-        csv_comp.loc[csv_comp.package.isin(['0402_C', '0402']), 'tip_lenty'] = "Tape08-2 white"
-        csv_comp = csv_comp[csv_comp['value']!='NM'].reset_index(drop=True)
         saved = False
-        print(f"    Кол-во ПКИ без CARRIER: {csv_comp['CarrierId'].isna().sum()}")
-        for comp in csv_comp.loc[csv_comp['CarrierId'].isna(), 'Comment'].to_list():
-            print(f"        {comp}")
-        csv_exists = csv_comp.dropna(subset=['CarrierId'])
-        csv_nonexistent = csv_comp[csv_comp['CarrierId'].isna()]
-        df_join_good = pd.concat([df_join_good, csv_exists], ignore_index=True, sort=False)
-        df_join_bad = pd.concat([df_join_bad, csv_nonexistent], ignore_index=True, sort=False)
-        while not saved:
-            try:
-                csv_exists.to_csv(OUT_FOLDER+'good_'+OUT_FILE, sep=';', index=False, header=True, encoding=ENCODING)
-                csv_nonexistent.to_csv(OUT_FOLDER+'bad_'+OUT_FILE, sep=';', index=False, header=True, encoding=ENCODING)
-                saved = True
-            except Exception as e:
-                print(f'Ошибка сохранения, возможно файл уже открыт:\n{e}')
-                sleep(1)
-            df_join_good.to_csv('READY/COMP_GOOD.csv', sep=';', index=False, header=True, encoding=ENCODING)
-            df_join_bad.to_csv('READY/COMP_BAD.csv', sep=';', index=False, header=True, encoding=ENCODING)
-
-
-        csv_pnp = csv_pnp.drop('CarrierId', axis=1)
-        saved = False
-        df_join = pd.concat([df_join, csv_pnp], ignore_index=True, sort=False)
         while not saved:
             try:
                 csv_pnp.to_csv(OUT_FOLDER+OUT_FILE, sep=';', index=False, header=True, encoding=ENCODING)
@@ -366,16 +248,6 @@ for i in range(len(argv)):
             except Exception as e:
                 print(f'Ошибка сохранения, возможно файл уже открыт:\n{e}')
                 sleep(1)
-            #df_join['Layer'] = '-'
-            #df_join.to_csv('READY/PNP_all.csv', sep=';', index=False, header=True)
-
-
-
-
-        ###############################################
-        ###############################################
-        ###############################################
-            
         print('\n\n\n-----------------------------------------------')
 try:
     pass
